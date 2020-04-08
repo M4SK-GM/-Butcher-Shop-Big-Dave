adeefb413296619d0075dcff05a99e90fcf39d09
Maxim <graff_makk@mail.ru>
2020-04-09 01:46:57 +0300

    First commit

---
+++ flask-app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+def main():
+    @app.route('/index')
+    def index():
+        return render_template('index.html', title='Основная')
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
